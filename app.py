76998cbb1d108a82acad6d442eaabf4cb4341eaf
Carmen Del Mastro <carmendiem2003@gmail.com>
2023-11-05 15:08:35 -0500

    display rows on home screen

--- app.py
+++ app.py
@@ -9,47 +9,45 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:carmen@localhost:
 app.config['SECRET_KEY'] = "carmen"
 db = SQLAlchemy(app)
 #flask run
-class Users(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-    
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
 
 class Transaction(db.Model):
     transaction_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, default=0)
-    date = db.Column(db.Date, default=func.now())
     amount = db.Column(db.Integer, nullable=False)
     category = db.Column(db.String(50), nullable=False)
     name = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, nullable=False) #default=func.now())
 
-    def __init__(self, amount, category, name):
+    def __init__(self, amount, category, name, date):
         self.amount = amount
         self.category = category
         self.name = name
+        self.date = date
 
 with app.app_context():
     db.create_all()
     
 
 @app.route('/')
-def hello_world():
-    return render_template('home_screen.html')
+def homepage():
+    trans = Transaction.query.all()
+    return render_template('home_screen.html', trans=trans)
 
 @app.route('/newtransaction', methods=['GET', 'POST'])
 def new_transaction():
-    current_time = datetime.now().strftime("%Y-%m-%d")
+    #current_time = datetime.now().strftime("%Y-%m-%d")
     if request.method == 'POST':
-        if not request.form['amount'] or not request.form['category'] or not request.form['name']:
+        if not request.form['amount'] or not request.form['category'] or not request.form['name'] or not request.form['date']:
             flash('Please enter all the fields', 'error')
         else:
-            new_transaction = Transaction(request.form['amount'], request.form['category'], request.form['name'])
+            date_str = request.form['date']
+            try:
+                 date = datetime.strptime(date_str, '%Y-%m-%d').date()
+            except ValueError:
+                 return "Invalid date format"
+            print(date)
+            new_transaction = Transaction(request.form['amount'], request.form['category'], request.form['name'], date=date)
             db.session.add(new_transaction)
             db.session.commit()
 
-            flash('Transaction added successfully!')
-            return redirect(url_for('hello_world'))
-    return render_template('add_transaction.html', current_time=current_time)
+            return redirect(url_for('homepage'))
+    return render_template('add_transaction.html') #, current_time=current_time)
